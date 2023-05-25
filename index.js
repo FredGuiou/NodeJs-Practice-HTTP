2785a452a0a9fca203787e69414638f79dec3a7d
FredGuiou <99122562+FredGuiou@users.noreply.github.com>
2023-05-25 08:43:51 +0200

    Init NPM project, linter & config, Undici - add function fetchData

---
+++ index.js
@@ -0,0 +1,31 @@
+"use Strict";
+
+// Import NodeJs dependencies
+import fs from "node:fs/promises";
+import path from "node:path";
+
+// Import des third party dependencies
+import { request } from "undici";
+
+// Effectuer une requête HTTP avec Undici.
+async function fetchData(url) {
+  try {
+    const { body } = await request(url);
+    const data = await body.json();
+
+    return data;
+  }
+  catch (error) {
+    throw new Error(`Request failed : status code ${error.status}`);
+  }
+}
+
+// Récupérer la dernière version d'un package depuis le registre des versions npm.
+
+// Comparer les versions et voir si c'est mineur ou majeur.
+
+// Actions sur Package.json (Principale fonction)
+// prévoir d'utiliser la fonction de requête HTTP.
+// prévoir d'utiliser la fonction de comparaison.
+
+// lancement de la principale fonction.
