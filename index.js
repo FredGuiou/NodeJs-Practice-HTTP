af4a328194aec7ad97c6958c0827379ba927642e
FredGuiou <99122562+FredGuiou@users.noreply.github.com>
2023-05-26 12:23:24 +0200

    Work in progress : function compareVersions - manage to log result true/false while using semver.gt

--- index.js
+++ index.js
@@ -7,9 +7,11 @@ import { fileURLToPath } from "url";
 
 // Import des third party dependencies
 import { request } from "undici";
+import semver from "semver";
+
 
 // Import des constantes
-const file = "package.json";
+const kFile = "package.json";
 
 // Effectuer une requête HTTP avec Undici.
 async function fetchData(url) {
@@ -29,21 +31,35 @@ async function getLatestVersion(name) {
   try {
     const npmRegistryUrl = `https://registry.npmjs.org/${name}`;
     const data = await fetchData(npmRegistryUrl);
+    const latestVersion = data["dist-tags"].latest;
+    console.log(latestVersion);
 
-    return data["dist-tags"].latest;
+    return latestVersion;
   }
   catch (error) {
     throw new Error(`Request failed : status code ${error.status}`);
   }
 }
 
+// Comparer deux versions avec le module npm semver
+async function compareVersions(latestVersion, localVersion) {
+  const comparator = semver.gt(latestVersion, localVersion);
+  console.log(comparator);
+}
+
+
 // Lire le fichier local package.json avec le module fs
 async function readPackageJson() {
   const __filename = fileURLToPath(import.meta.url);
   const __dirname = path.dirname(__filename);
-  const result = await fs.readFile(path.join(__dirname, "fixtures", `${file}`), { encoding: "utf-8" });
+  const result = await fs.readFile(path.join(__dirname, "fixtures", `${kFile}`), { encoding: "utf-8" });
   const { dependencies } = JSON.parse(result);
-  console.log({ dependencies });
+  console.log(dependencies);
+
+  return dependencies;
 }
 
-readPackageJson();
+// Lancement de la fonction
+// getLatestVersion("pg");
+// readPackageJson();
+compareVersions("9.8.7", "1.2.3");
