22dab2951fb2a4668e0744072c87abf9efc1cf7e
FredGuiou <99122562+FredGuiou@users.noreply.github.com>
2023-05-30 17:50:37 +0200

    Mod : Test fetchData function with Mockagent

--- test/test.js
+++ test/test.js
@@ -4,27 +4,44 @@
 import { describe, it } from "node:test";
 import assert from "node:assert";
 
+// Import des third party dependencies
+import { MockAgent, setGlobalDispatcher } from "undici";
+
 // Import des constantes et fonctions à tester
-import npmTest from "../index.js";
+import * as npmTest from "../index.js";
+
+// Import des constantes
+const kNpmApiUrl = "https://registry.npmjs.org";
+
+// Paramétrage du MockAgent
+const mockAgent = new MockAgent();
+mockAgent.disableNetConnect();
+const mockPool = mockAgent.get(kNpmApiUrl);
+const mockResult = { result: "ok" };
+mockPool.intercept({ path: "/pg" }).reply(200, mockResult, {
+  headers: { "Content-Type": "application/json" }
+}).persist();
+mockPool.intercept({ path: "/toto" }).reply(500, {}, {
+  headers: { "Content-Type": "application/json" }
+}).persist();
+setGlobalDispatcher(mockAgent);
 
 
 describe("fetchData function", () => {
   it("Should return a status Code 200 for \"OK\"", async() => {
-    const testUrl = "https://registry.npmjs.org/";
-    const result = await npmTest.fetchData(testUrl);
-    assert.strictEqual(result.statusCode, 200);
+    const result = await npmTest.fetchData(`${kNpmApiUrl}/pg`);
+    assert.equal(result.statusCode, 200);
   });
   it("Should return a body", async() => {
-    const testUrl = "https://registry.npmjs.org/";
-    const result = await npmTest.fetchData(testUrl);
-    assert.strictEqual(Object.prototype.hasOwnProperty.call(result, "body"), true);
+    const result = await npmTest.fetchData(`${kNpmApiUrl}/pg`);
+    assert.deepEqual(result.body, mockResult);
   });
   it("Should throw an error if request failed", async() => {
-    const wrongTestUrl = "https://wrong.registry.npmjs.org/";
     await assert.rejects(async() => {
-      await npmTest.fetchData(wrongTestUrl);
+      await npmTest.fetchData(`${kNpmApiUrl}/toto`);
     }, {
-      name: "Error"
+      name: "Error",
+      message: "HTTP request failed : status code 500"
     });
   });
 });
