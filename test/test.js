eabfb2c679be7ac8780e4352524f2b2d50063903
FredGuiou <99122562+FredGuiou@users.noreply.github.com>
2023-05-30 13:38:07 +0200

    Adding test folder and test.js - Work in progress

---
+++ test/test.js
@@ -0,0 +1,54 @@
+"use Strict";
+
+// Import des nodeJs dependencies
+import { describe, it } from "node:test";
+import assert from "node:assert";
+
+// Import des constantes et fonctions à tester
+import { kFile,
+  fetchData,
+  getLatestVersion,
+  compareVersions,
+  readPackageJson,
+  getPackages,
+  displayGetPackages } from "../index";
+
+
+describe("fetchData function", () => {
+  it("Should return a status Code 200 for well completed ", () => {
+
+  });
+
+});
+
+describe("getLatestVersion function", () => {
+  it("", () => {
+
+  });
+
+});
+
+describe("compareVersions function", () => {
+  it("", () => {
+
+  });
+
+});
+describe("readPackageJson function", () => {
+  it("", () => {
+
+  });
+
+});
+describe("getPackages function", () => {
+  it("", () => {
+
+  });
+
+});
+describe("displayGetPackages function", () => {
+  it("", () => {
+
+  });
+
+});
